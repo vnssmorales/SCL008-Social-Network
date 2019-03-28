00965cfe244c645565919f35fe71625706316f1c
Vilantra <karen.reyes.c@gmail.com>
2019-03-27 23:22:28 -0300

    Updates

---
+++ assets/views/templateCreate.js
@@ -0,0 +1,27 @@
+import{createAccount} from './../js/auth.js';
+
+/*
+2- Crear la plantilla estructural del html que representa al login
+Debemos importar la funcion login google
+*/
+
+//import {loginGoogle} from './../js/auth.js'
+
+export const templateCreate =()=>{
+// creamos div que contendrá la plantilla
+    const containerCreate = document.createElement('div');
+    // creamos el contenido dellogin
+    const contentCreate = `<p>Crear Cuenta</p>
+                            <button id="create">Create</button>`
+//pasar el contenido al div
+    containerCreate.innerHTML=contentCreate;
+//le pido que busque el id del bton dentro del div creado
+    const btn=containerCreate.querySelector('#create');
+//evento del boton que llama a la autentificacion de google
+
+    btn.addEventListener('click',()=>{
+        console.log(createAccount());
+    })
+    return containerCreate;
+
+}
---
+++ route.js
@@ -0,0 +1,51 @@
+import {templateLogin} from './assets/views/templateLogin.js'
+import {templateCreate} from './assets/views/templateCreate.js'
+
+
+/*
+crear una funcion que reciba el hast y segun el match retorne otra funcion que va a imprimir el template en el html
+*/
+
+const changeRouter = (hash) =>{
+    if (hash==='#/login'){
+        return showTemplate(hash);
+    }
+    if (hash==='#/create'){
+        return showTemplate(hash);
+    }
+}
+//imprimira el template en el html
+const showTemplate = (hash) =>{
+const router=hash.substring(2);
+let containterRoot = document.getElementById("root");
+containterRoot.innerHTML='';
+
+//hacemos el match del hash utilizado y el template que quiero mostrar
+
+switch(router){
+case 'login':
+containterRoot.appendChild(templateLogin());
+break;
+case 'create':
+containterRoot.appendChild(templateCreate());
+break;
+default:
+containterRoot.innerHTML = `<p>Error 408</p>`
+}
+}
+
+//inicializar las rutas para que se ejecute changeRoute() ye en su defecto showTemplate()
+
+export const initRouter=()=>{
+
+    window.addEventListener('load',changeRouter(window.location.hash));
+
+    //reconoce un cambio en el hash y le pasa el nuevo hash a changRouter
+
+    if('onhashchange'in window){
+        window.onhashchange=()=>{
+            changeRouter(window.location.hash);
+        }
+    }
+
+}
