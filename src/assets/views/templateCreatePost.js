fe7ef5add9913bbf44aec127c4ae13b77a4e14e2
TatianaCastroLizama <tcl.contacto@gmail.com>
2019-04-01 15:34:56 -0300

    template

---
+++ src/assets/views/templateCreatePost.js
@@ -0,0 +1,11 @@
+import{createPost} from './../js/auth.js';
+
+export const templateCreatePost =() => {
+  
+    const containerCreatePost = document.createElement('div');
+
+    const contentCreatePost = `ENLAZAR A TEMPLATE`
+
+    containerCreatePost.innerHTML = contentCreatePost;
+    
+}
