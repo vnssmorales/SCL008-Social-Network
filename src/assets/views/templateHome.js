4626a6e990e38e5cd6e43093326ada702c5ba055
vnssmorales <vnss.morales@gmail.com>
2019-04-12 10:17:32 -0400

    mobile first listo

--- src/assets/views/templateHome.js
+++ src/assets/views/templateHome.js
@@ -4,14 +4,14 @@ export const templateHome =()=>{
 
     const contentHome = `<main>
     <div class="container" id="infofoto">
-    <div class="row ">
-    
+    <div class="row around-xs">
+    <div class="foto col-xs-10 col-sm-8 col-md-8">
     <h4>Te invitamos a usar la nueva aplicación para saber todo sobre lugares de Chile, actividades, hospedaje, y
         tips para preparar tu viaje.
         Si es primera vez que viajas, inicia una nueva conversación dónde puedes pedir recomendaciones para tu
         primer viaje.
         Si ya eres viajero frecuente, comparte tus historias y datos imperdibles!</h4>
-    <div class="foto col-xs-3">
+    
         <img src="./img/ViajaxChile.jpg" alt="imagen chile">
         </div>
     </div>
